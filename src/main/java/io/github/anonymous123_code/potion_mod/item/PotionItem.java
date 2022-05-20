51a534f7a7d413d23a2ad43ce66378c9db38ad9e
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-05-20 16:37:32 +0200

    Move evaluatePotion to Utility class

--- src/main/java/io/github/anonymous123_code/potion_mod/item/PotionItem.java
+++ src/main/java/io/github/anonymous123_code/potion_mod/item/PotionItem.java
@@ -1,12 +1,7 @@
 package io.github.anonymous123_code.potion_mod.item;
 
 import io.github.anonymous123_code.potion_mod.PotionMod;
-import io.github.anonymous123_code.potion_mod.api.data_type.Data;
-import io.github.anonymous123_code.potion_mod.api.operator.ArgumentExecutingOperator;
-import io.github.anonymous123_code.potion_mod.api.operator.Operator;
-import io.github.anonymous123_code.potion_mod.api.operator.OperatorRegistry;
-import io.github.anonymous123_code.potion_mod.api.operator.RawArgumentOperator;
-import io.github.anonymous123_code.potion_mod.data_type.VoidDataFactory;
+import io.github.anonymous123_code.potion_mod.PotionUtility;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -14,16 +9,11 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A PotionMod potion. The Vanilla one just isn't made for this type of system
@@ -53,7 +43,7 @@ public class PotionItem extends net.minecraft.item.PotionItem {
 		if (!world.isClient && stack.hasNbt()) {
 			NbtCompound compound = stack.getNbt().getCompound("Potion");
 			if (compound != null) {
-				PotionMod.LOGGER.info(evaluatePotion(compound, 0).toString());
+				PotionMod.LOGGER.info(PotionUtility.evaluatePotion(compound, 0).toString());
 			}
 		}
 
@@ -66,21 +56,5 @@ public class PotionItem extends net.minecraft.item.PotionItem {
 
 		world.emitGameEvent(user, GameEvent.DRINKING_FINISH, user.getCameraBlockPos());
 		return stack;
-	}
-
-	public Data<?> evaluatePotion(NbtCompound potion, int rec_depth) {
-		Operator<?> operator = OperatorRegistry.get(new Identifier(potion.getString("operator")));
-		if (operator instanceof ArgumentExecutingOperator) {
-			List<Data<?>> params = new ArrayList<>();
-			for (NbtElement element : potion.getList("parameters", 10)) {
-				params.add(evaluatePotion((NbtCompound) element, rec_depth + 1));
-			}
-			return ((ArgumentExecutingOperator) operator).getResult(params);
-		} else if (operator instanceof RawArgumentOperator) {
-			return ((RawArgumentOperator) operator).getResult(potion.getList("parameters", 10));
-		} else {
-			// This should never happen, sealed is used
-			return VoidDataFactory.getInstance().create(null);
-		}
 	}
 }
