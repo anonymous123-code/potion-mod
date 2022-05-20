2a35535cef19fc8a5c5bca63031ba5e63f3766d3
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-05-20 16:55:33 +0200

    Register Escape Operator

--- src/main/java/io/github/anonymous123_code/potion_mod/PotionMod.java
+++ src/main/java/io/github/anonymous123_code/potion_mod/PotionMod.java
@@ -2,9 +2,11 @@ package io.github.anonymous123_code.potion_mod;
 
 import io.github.anonymous123_code.potion_mod.api.operator.OperatorRegistry;
 import io.github.anonymous123_code.potion_mod.data_type.AmountDataFactory;
+import io.github.anonymous123_code.potion_mod.data_type.PotionDataFactory;
 import io.github.anonymous123_code.potion_mod.data_type.VoidDataFactory;
 import io.github.anonymous123_code.potion_mod.item.PotionItem;
 import io.github.anonymous123_code.potion_mod.operators.AmountOperator;
+import io.github.anonymous123_code.potion_mod.operators.EscapeOperator;
 import io.github.anonymous123_code.potion_mod.operators.NegativityOperator;
 import io.github.anonymous123_code.potion_mod.operators.SelectionOperator;
 import net.minecraft.util.Identifier;
@@ -31,6 +33,7 @@ public class PotionMod implements ModInitializer {
 		OperatorRegistry.register(new AmountOperator(new Identifier(mod.metadata().id(), "amount")));
 		OperatorRegistry.register(new NegativityOperator(new Identifier(mod.metadata().id(), "negativity")));
 		OperatorRegistry.register(new SelectionOperator(new Identifier(mod.metadata().id(), "selection")));
+		OperatorRegistry.register(new EscapeOperator(new Identifier(mod.metadata().id(), "escape")));
 
 		VoidDataFactory.setUp(new Identifier(mod.metadata().id(), "void"));
 		AmountDataFactory.setUp(new Identifier(mod.metadata().id(), "amount"));
