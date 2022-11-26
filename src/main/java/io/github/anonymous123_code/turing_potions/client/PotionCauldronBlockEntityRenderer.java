5d288633077c821b9d60283ce33aedae79b14565
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-11-26 12:03:45 +0100

    Potion Color animation

--- src/main/java/io/github/anonymous123_code/turing_potions/client/PotionCauldronBlockEntityRenderer.java
+++ src/main/java/io/github/anonymous123_code/turing_potions/client/PotionCauldronBlockEntityRenderer.java
@@ -11,6 +11,7 @@ import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 
@@ -35,30 +36,38 @@ public class PotionCauldronBlockEntityRenderer implements BlockEntityRenderer<Po
 		Sprite potionSprite =  potionSpriteIdentifier.getSprite();
 		VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getTranslucent());
 
+		float time = (entity.getWorld().getTime() + tickDelta);
+		time /= 4;
+		float colorRed = .7f+MathHelper.sin(time/8)*0.1f+MathHelper.sin(time/3)*0.05f;
+		time = time * 4 / 5;
+		float colorGreen = .5f+MathHelper.sin(time/8)*0.1f+MathHelper.sin(time/3)*0.05f;
+		time = time * 7 / 9;
+		float colorBlue = Math.min(1f+MathHelper.sin(time/8)*0.1f+MathHelper.sin(time/3)*0.05f, 1f);
+
 		vertexConsumer
 				.vertex(matrices.peek().getModel(), 0.125f, .5625f + (entity.getLength()-1)*3/16f, 0.875f)
-				.color(.7f, .5f, 1f, 1f)
+				.color(colorRed, colorGreen, colorBlue, 1f)
 				.uv(potionSprite.getMinU(), potionSprite.getMaxV())
 				.light(0,240)
 				.normal(0,1,0).next();
 
 		vertexConsumer
 				.vertex(matrices.peek().getModel(), 0.875f, .5625f + (entity.getLength()-1)*3/16f, 0.875f)
-				.color(.7f, .5f, 1f, 1f)
+				.color(colorRed, colorGreen, colorBlue, 1f)
 				.uv(potionSprite.getMaxU(), potionSprite.getMaxV())
 				.light(0,240)
 				.normal(0,1,0).next();
 
 		vertexConsumer
 				.vertex(matrices.peek().getModel(), 0.875f, .5625f + (entity.getLength()-1)*3/16f, 0.125f)
-				.color(.7f, .5f, 1f, 1f)
+				.color(colorRed, colorGreen, colorBlue, 1f)
 				.uv(potionSprite.getMaxU(), potionSprite.getMinV())
 				.light(0,240)
 				.normal(0,1,0).next();
 
 		vertexConsumer
 				.vertex(matrices.peek().getModel(), 0.125f, .5625f + (entity.getLength()-1)*3/16f, 0.125f)
-				.color(.7f, .5f, 1f, 1f)
+				.color(colorRed, colorGreen, colorBlue, 1f)
 				.uv(potionSprite.getMinU(), potionSprite.getMinV())
 				.light(0,240)
 				.normal(0,1,0)
